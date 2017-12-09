bd80f9dd0f3f386964b9efba9877f8e06803d3a3
Martin-MacDonald <mmacdo54@caledonian.ac.uk>
2017-12-09 00:08:39 +0000

    Updated basic sim

--- src/GameSimulator.java
+++ src/GameSimulator.java
@@ -7,20 +7,25 @@ public class GameSimulator{
 	private List<GameChoices> gameChoiceList = Arrays.asList(GameChoices.values());
 	
 	private int checkGameResult(GameChoices myChoice, GameChoices theirChoice){
+
+		if(myChoice == theirChoice) return 0;
 		
-		int myChoicePos = myChoice.ordinal();
-		int theirChoicePos = theirChoice.ordinal();
-		int sumOfChoices = myChoicePos + theirChoicePos;
-		
-		System.out.println(myChoicePos);
-		System.out.println(theirChoicePos);
-		
-		switch(myChoicePos){
-			case 0: if(sumOfChoices == 1 || sumOfChoices == 4) return 2; break;
-			case 1:	if(sumOfChoices == 3 || sumOfChoices == 4) return 2; break;
-			case 2:	if(sumOfChoices == 2 || sumOfChoices == 6) return 2; break;
-			case 3:	if(sumOfChoices == 3 || sumOfChoices == 5) return 2; break;
-			case 4:	if(sumOfChoices == 5 || sumOfChoices == 7) return 2; break;
+		switch(myChoice){
+			case ROCK:
+				if(theirChoice == GameChoices.PAPER || theirChoice == GameChoices.SPOCK) return 2; 
+				break;
+			case PAPER:
+				if(theirChoice == GameChoices.SCISSORS || theirChoice == GameChoices.LIZARD) return 2; 
+				break;
+			case SCISSORS: 
+				if(theirChoice == GameChoices.ROCK || theirChoice == GameChoices.SPOCK) return 2; 
+				break;
+			case LIZARD:
+				if(theirChoice == GameChoices.ROCK || theirChoice == GameChoices.SCISSORS) return 2; 
+				break;
+			case SPOCK:
+				if(theirChoice == GameChoices.PAPER || theirChoice == GameChoices.LIZARD) return 2; 
+				break;
 		}
 		
 		return 1;
